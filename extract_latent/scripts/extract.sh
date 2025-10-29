8eabf63a0708767e30ca00482f6e0eb992ebc1e1
ProgramerSalar <manishkumar60708090@gmail.com>
2025-10-29 12:29:31 +0530

    yaah file format to be changed

--- extract_latent/scripts/extract.sh
+++ extract_latent/scripts/extract.sh
@@ -1,7 +1,7 @@
 GPUS=1
-ANNO_FILE=/home/manish/Desktop/projects/vae_from_scratch/annotation/video_text.jsonl
+ANNO_FILE=../../vae_from_scratch/annotation/video_text.jsonl
 torchrun --nproc_per_node $GPUS \
-    /home/manish/Desktop/projects/vae_from_scratch/extract_latent/extract_video_latent.py \
+    ../../vae_from_scratch/extract_latent/extract_video_latent.py \
     --model_dtype bf16 \
     --batch_size 1 \
     --anno_file $ANNO_FILE \
