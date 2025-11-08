cc876149dc62f9d8f46db71e8c395ce703af1998
ProgramerSalar <manishkumar60708090@gmail.com>
2025-11-08 13:30:26 +0530

    Testing Testing Testing..

--- video_vae/train/main.py
+++ video_vae/train/main.py
@@ -13,20 +13,40 @@ from middleware.scheduler import cosine_scheduler
 
 
 
-
 def main(args):
   device = torch.device("cuda:0")
   train_video_dataloaders = Video_dataloader(args=args)
-  train_video_dataloaders = next(iter(train_video_dataloaders))
+  train_video_dataloaders = next(iter(train_video_dataloaders)).to(device)
+  model = CausalVideoLossWrapper(num_groups=args.batch_size, args=args).to(device)
+  optimizer = create_optimizer(args=args,
+                                 model=model)
+  
+  scaler = torch.amp.GradScaler(device="cuda")
 
 
   for epoch in range(100):
-    model = CausalVideoLossWrapper(num_groups=args.batch_size, args=args).to(device)
-    # optimizer = create_optimizer(args=args,
-    #                              model=model.vae)
-      
-    rec_loss, gan_loss, log_loss = model(train_video_dataloaders, args.global_step)
+    optimizer.zero_grad()
 
+    # the reconstruct loss 
+    with torch.amp.autocast(device_type="cuda", dtype=torch.float16):
+      rec_loss, gan_loss, log_loss = model(train_video_dataloaders, epoch)
+      print(rec_loss, gan_loss, log_loss)
+
+    scaler.scale(rec_loss).backward()
+    scaler.unscale_(optimizer)
+    total_grad_norm = torch.nn.utils.clip_grad_norm_(parameters=model.parameters(),
+                                                      max_norm=1.0)
+    print(f"what is the value have total_grad_norm: >>>>>>>>>>>>>> {total_grad_norm}")
+    scaler.step(optimizer)
+    scaler.update()
+
+
+
+
+
+    
+      
+      
     
 
 
